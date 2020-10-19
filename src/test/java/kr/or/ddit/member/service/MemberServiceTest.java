9c142df0c79ce8b7611ed63783e46431368364b2
dam <qkrekal1389@gmail.com>
2020-10-19 09:19:18 +0900

    실습  . pages 속성을 통해 페이지 내비게이션 생성 작업  . pageSize 파라미터 처리하기
    JSTL
     . 국제화 - bundle, message, setBundle
    
    실습
     . select box 변경시 언어 변경

--- src/test/java/kr/or/ddit/member/service/MemberServiceTest.java
+++ src/test/java/kr/or/ddit/member/service/MemberServiceTest.java
@@ -2,10 +2,14 @@ package kr.or.ddit.member.service;
 
 import static org.junit.Assert.*;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import kr.or.ddit.common.model.PageVO;
 import kr.or.ddit.member.dao.MemberDao;
@@ -13,7 +17,7 @@ import kr.or.ddit.member.dao.MemberDaoI;
 import kr.or.ddit.member.model.MemberVO;
 
 public class MemberServiceTest {
-
+	private static final Logger logger = LoggerFactory.getLogger(MemberServiceTest.class);
 	@Test
 	public void getMemberTest() {
 		/***Given***/
@@ -28,10 +32,10 @@ public class MemberServiceTest {
 		MemberVO memberVO = memberService.getMember(userId);
 
 		/***Then***/
-//		assertEquals("brown", memberVO.getUserId());
-//		assertEquals("passBrown", memberVO.getPassword());
+		assertEquals("brown", memberVO.getUserid());
+		assertEquals("brownPass", memberVO.getPass());
 
-		assertEquals(answerMemberVO, memberVO);
+//		assertEquals(answerMemberVO, memberVO);
 	}
 
 	
@@ -46,10 +50,12 @@ public class MemberServiceTest {
 
 		/***When***/
 		Map<String, Object> map = memberService.selectAllMemberPage(pageVO);
-		List<MemberVO> memberListPage = (List<MemberVO>)map.get("memList");
+		List<MemberVO> memberListPage = (List<MemberVO>)map.get("memberList");
+		int pages = (int)map.get("pages");
 
 		/***Then***/
 		assertEquals(7, memberListPage.size());
+		assertEquals(3, pages);
 		
 	}
 	
@@ -65,4 +71,11 @@ public class MemberServiceTest {
 		assertEquals(15, totalCount);
 	}
 
+	@Test
+	public void localeListTest() {
+		Locale[] locales = SimpleDateFormat.getAvailableLocales();
+		for(Locale locale : locales) {
+			logger.debug(locale.toString());
+		}
+	}
 }
