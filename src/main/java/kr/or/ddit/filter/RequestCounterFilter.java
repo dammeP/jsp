5aaa8e90c1cb366e23f53290c3dd72fe7975268d
dam <qkrekal1389@gmail.com>
2020-10-19 20:08:26 +0900

    getRequestResponse -> 잘못된파일, requestResponse 안의 form action="${pageContext.request.contextPath }/request/requestResponse.jsp" 변경

--- src/main/java/kr/or/ddit/filter/RequestCounterFilter.java
+++ src/main/java/kr/or/ddit/filter/RequestCounterFilter.java
@@ -64,7 +64,13 @@ public class RequestCounterFilter implements Filter{
 		
 		// 등록된 다른 필터로 요청 위임
 		// 만약에 더이상 등록된 Filter가 없을경우 요청을 처리할 서블릿 / jsp으로 요청을 전달
-		chain.doFilter(request, response);
+		
+		전처리
+		
+		chain.doFilter(request, response); // servlet 처리
+		
+		후처리 - servlet 응답생성후 응답이 웹브라우저로 가는단계에서 후속처리
+		
 	}
 
 	@Override
